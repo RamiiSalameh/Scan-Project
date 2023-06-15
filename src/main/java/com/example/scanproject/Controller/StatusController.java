079f7dc33bbd4b0efc481d41a32a190584ba8d42
rami_salameh <rami.slameh@gmail.com>
2023-06-15 19:36:24 +0300

    initial push

--- src/main/java/com/example/scanproject/Controller/StatusController.java
+++ src/main/java/com/example/scanproject/Controller/StatusController.java
@@ -3,59 +3,43 @@ package com.example.scanproject.Controller;
 import com.example.scanproject.Data.Scan;
 import com.example.scanproject.Data.ScanRepository;
 import com.example.scanproject.Data.ScanStatus;
+import com.example.scanproject.Process.ScanProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.Optional;
-import java.util.concurrent.RejectedExecutionException;
 
-@Component
-public class ScanProcessor {
-
-    private final ThreadPoolTaskExecutor taskExecutor;
+@RestController
+@RequestMapping("/status")
+public class StatusController {
 
     @Autowired
     private ScanRepository scanRepository;
 
-    public ScanProcessor() {
-        taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(1); // Set the desired number of threads for processing
-        taskExecutor.setMaxPoolSize(5); // Set the maximum number of threads for processing
-        taskExecutor.setThreadNamePrefix("ScanProcessor-");
-        taskExecutor.initialize();
-    }
+    @GetMapping("/{scanId}")
+    public ResponseEntity<String> getStatus(@PathVariable String scanId) {
+        long ScanIdLong = 0L;
+        try{
+            ScanIdLong = Long.parseLong(scanId);
+        }catch (NumberFormatException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ScanStatus.NOT_FOUND.toString());
 
-    public void processScan(Long scanId) {
-        try {
-            taskExecutor.execute(() -> {
-                // Fetch the scan from the database
-                Optional<Scan> scanOptional = scanRepository.findById(scanId);
-                if (scanOptional.isPresent()) {
-                    Scan scan = scanOptional.get();
-                    // Perform the necessary processing tasks for the scan
-
-                    // Update the scan status (e.g., mark it as running or complete)
-                    scan.setStatus(ScanStatus.RUNNING);
-                    scanRepository.save(scan);
-
-                    // Simulate the scan process
-                    try {
-                        Thread.sleep(5000); // Simulating the scan process
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    // Update the scan status to complete
-                    scan.setStatus(ScanStatus.COMPLETE);
-                    scanRepository.save(scan);
-                }
-            });
-        } catch (RejectedExecutionException e) {
-            // Handle rejection if the task cannot be accepted by the thread pool
-            // This can happen if the thread pool is full or has reached its maximum capacity
-            // You can decide how to handle such scenarios based on your requirements
-            e.printStackTrace();
+        }
+        Optional<Scan> scanOptional = scanRepository.findById(ScanIdLong);
+        if (scanOptional.isPresent()) {
+            ScanStatus status = scanOptional.get().getStatus();
+            return ResponseEntity.ok(status.toString());
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
+
 }
+
+
